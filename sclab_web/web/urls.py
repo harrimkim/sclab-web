88e4622f189bddd7728a683355b7eb9f38b1b70f
harrimkim <harrimkim@gmail.com>
2018-06-20 15:31:31 +0900

    static please

--- sclab_web/web/urls.py
+++ sclab_web/web/urls.py
@@ -4,9 +4,9 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/professor', views.professor, name='professor'),
-    path('/members', views.members, name='members'),
-    path('/news', views.news, name='news'),
-    path('/publication', views.publication, name='publication'),
-    path('/project', views.project, name='project'),
+    path('professor', views.professor, name='professor'),
+    path('members', views.members, name='members'),
+    path('news', views.news, name='news'),
+    path('publication', views.publication, name='publication'),
+    path('project', views.project, name='project'),
 ]
