ec510b498b1bc7dc93aeb712f303389e1eac3f60
Ubuntu <ubuntu@ip-172-31-29-13.ap-northeast-2.compute.internal>
2018-06-27 16:09:38 +0000

    update

--- sclab_web/web/models.py
+++ sclab_web/web/models.py
@@ -71,7 +71,7 @@ class Publication(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     journal = models.CharField(max_length=200)
-    date = models.CharField(max_length=200)
+    date = models.CharField(max_length=200, default='')
 
     def __str__(self):
         return self.title
@@ -84,3 +84,4 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+
