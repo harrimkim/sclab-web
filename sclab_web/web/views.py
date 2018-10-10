5023d46c12c0a5765e2c6b9c397fcb5cba78d52c
jeongjaeyong <42512062+jeongjaeyong@users.noreply.github.com>
2018-10-10 22:14:40 +0900

    Update views.py

--- sclab_web/web/views.py
+++ sclab_web/web/views.py
@@ -32,11 +32,12 @@ def posts(request, id):
 
 def publication(request):
     publications = Publication.objects.values('date').annotate(Count('date'))
+    links = Publication.objects.values.all()
     for pub in publications:
         d = dict()
         d['pub'] = Publication.objects.filter(date=pub['date'])
         pub.update(d)
-    return render(request, 'web/publication.html', {'publications' : publications[::-1]})
+    return render(request, 'web/publication.html', {'publications' : publications[::-1], 'links':links})
 
 def project(request):
     projects = Project.objects.all().order_by('-id')
