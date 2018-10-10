40e40b13333a92ce74231890d188725e56c390ea
jeongjaeyong <42512062+jeongjaeyong@users.noreply.github.com>
2018-10-10 22:31:56 +0900

    Update models.py

--- sclab_web/web/models.py
+++ sclab_web/web/models.py
@@ -60,6 +60,7 @@ class Member(models.Model):
 class Link(models.Model):
     title = models.CharField(max_length=200)
     link = models.CharField(max_length=200, default='')
+    test = models.CharField(max_length=200, default='')
     def __str__(self):
         return self.title
     
