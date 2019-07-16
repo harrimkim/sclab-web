097e579814116bd521c5c18dad8b23afff979c84
john980302 <40467774+john980302@users.noreply.github.com>
2019-07-16 11:49:17 +0900

    Update settings.py

--- sclab_web/config/settings.py
+++ sclab_web/config/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'ec2-13-125-207-49.ap-northeast-2.compute.amazonaws.com',
+    'ec2-54-180-121-244.ap-northeast-2.compute.amazonaws.com',
     '.kro.kr',
 ]
 
